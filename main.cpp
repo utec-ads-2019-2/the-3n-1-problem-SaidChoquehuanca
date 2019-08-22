cb3dff9e9fcf7d83aa6eeb8bcde597096e21f130
SaidChoquehuanca <39719094+SaidChoquehuanca@users.noreply.github.com>
2019-08-22 18:06:51 -0500

    exercise

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+int algoritm(int numero,int cycles){
+    if(numero==1){
+        return cycles+1;
+    } else if(numero%2==1){
+        algoritm((numero*3)+1,cycles+1);
+    } else{
+        algoritm(numero/2,cycles+1);
+    }
+}
+
+
 int main(int argc, char *argv[]) {
-    return 0;
+    int inicio,final,mayor=0;
+    int k=0;
+    while(k!=1) {
+        cin >> inicio >> final;
+        for (int i = inicio; i < final; i++) {
+            if (algoritm(i, 0) > mayor)
+                mayor = algoritm(i, 0);
+        }
+        cout << inicio << " " << final << " " << mayor<<"\n";
+    }
 }
